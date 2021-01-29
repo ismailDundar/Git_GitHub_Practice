04071dde33b9f7aa04eb88eee5b17bb479e2c1b5
ismail dundar <dundar.sml@gmail.com>
2021-01-29 11:25:05 +0100

    project is already in remote repo, committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,11 @@ public class Hello {
     // more stuff
     // Mac user and windows users are happy to see commit dialog finally
 
+
+    // I have already published on github.com
+    // now I am ready to make more commits
+    // but do I need to share again ????
+    //ABSOLUTELY NO
+    //IT'S TIME TO COMMIT AND JUST PUSH !!!!!!
+
 }
